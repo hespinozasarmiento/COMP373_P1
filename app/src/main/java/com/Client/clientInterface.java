6d5a3aa4b95a0624eec5ed5d071d9a1aad73adf1
henryespinoza <hespinozasarmiento@luc.edu>
2020-02-22 23:56:07 -0600

    Code for client interface set up.

--- app/src/main/java/com/Client/clientInterface.java
+++ app/src/main/java/com/Client/clientInterface.java
@@ -1,4 +1,15 @@
 package com.Client;
 
 public class clientInterface {
+    /*
+     * Choose one of the following client interfaces to run and comment out the other two client interface.
+     */
+    
+    public static void main(String[] args) throws Exception {
+
+        FacilityClient facilityClient = new FacilityClient();
+        //MaintenanceClient maintenanceClient = new MaintenanceClient();
+        //UsageClient usageClient = new UsageClient();
+
+    }
 }
